249cbedd4ac68a055c6a40adc9196f5db2a6c1d9
jairorjuela <genius85862@gmail.com>
2018-08-19 16:26:05 -0500

    Añadiendo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.find_or_create_by(name: "Restaurantes")
+
+Category.find_or_create_by(name: "Tienda")
+
+Category.find_or_create_by(name: "Car")
+
+Category.find_or_create_by(name: "Servicios")
+
+Category.find_or_create_by(name: "Hogar")
+
+Category.find_or_create_by(name: "Educación")
+
+Category.find_or_create_by(name: "Diversión")
+
+Category.find_or_create_by(name: "Viaje")
+
+Purchase.find_or_create_by(name_purchases: "Compra")
+
+Purchase.find_or_create_by(name_purchases: "Retiro")
+
+Purchase.find_or_create_by(name_purchases: "Transferencia")
+
+Purchase.find_or_create_by(name_purchases: "Pago")
