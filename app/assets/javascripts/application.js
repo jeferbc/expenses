5752f8dce3d59250caa4559df9d0508faa924fb6
jairorjuela <genius85862@gmail.com>
2018-08-23 23:03:03 -0500

    gem chartkick and highchats

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,9 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require highcharts
+//= require Chart.bundle
+//= require chartkick
 //= require_tree .
 
 $(document).ajaxError(function(event,xhr,options,exc) {
