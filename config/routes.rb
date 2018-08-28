d95ca35efeee916d631878ccb49eb33a12cc17a3
jairorjuela <genius85862@gmail.com>
2018-08-27 21:37:56 -0500

    Corregir error de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-     resources :expenses
+      resources :expenses, only: [:index, :create, :update, :destroy]
     end
   end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
