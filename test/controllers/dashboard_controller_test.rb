a7db87dafa691d20e99332d7988c3fc40974d7dc
jairorjuela <genius85862@gmail.com>
2018-09-28 23:31:59 -0500

    Gem minitest-reporters and test category

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get dashboard_index_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
+  #end
 
 end
