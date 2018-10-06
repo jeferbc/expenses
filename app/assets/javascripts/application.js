1e314bd32061a6619263cc1d16e93501c910243e
jairorjuela <genius85862@gmail.com>
2018-10-06 18:40:49 -0500

    Heroku 9

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,15 +20,14 @@
 //= require morris
 //= require_tree .
 
-$(document).ajaxError(function(event,xhr,options,exc) {
+$(document).ajaxError(function (event, xhr, options, exc) {
 
     var errors = JSON.parse(xhr.responseText);
-    var er ="<ul>";
-    for(var i = 0; i < errors.length; i++){
+    var er = "<ul>";
+    for (var i = 0; i < errors.length; i++) {
         var list = errors[i];
-        er += "<li>"+list+"</li>"
+        er += "<li>" + list + "</li>";
     }
-    er+="</ul>"
+    er += "</ul>";
     $("#error_explanation").html(er);
-
 });
