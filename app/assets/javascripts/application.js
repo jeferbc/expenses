ddb1cde5ceb881e10f5611e4932f803ad4c5869d
jairorjuela <genius85862@gmail.com>
2018-10-20 19:26:00 -0500

    Heroku 20

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,17 +16,15 @@
 //= require turbolinks
 //= require Chart.bundle
 //= require chartkick
-//= require raphael
-//= require morris
+//= require_tree 
 
 $(document).ajaxError(function (event, xhr, options, exc) {
-
     var errors = JSON.parse(xhr.responseText);
     var er = "<ul>";
     for (var i = 0; i < errors.length; i++) {
         var list = errors[i];
         er += "<li>" + list + "</li>";
-    }
+    };
     er += "</ul>";
     $("#error_explanation").html(er);
 });
