ea6541e3e2fb2d3082efbb5d6880d7fb059bca4d
jairorjuela <genius85862@gmail.com>
2018-10-22 17:43:41 -0500

    Heroku 23 add line to /environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
+Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb)
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
