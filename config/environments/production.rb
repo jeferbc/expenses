36be40ada5ad39caf1aeb1264aa265269f358f5a
jairorjuela <genius85862@gmail.com>
2018-10-22 19:21:51 -0500

    Heroku 24 environments/production.rb assets.config=true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,9 +21,9 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb)
+  Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb)
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
